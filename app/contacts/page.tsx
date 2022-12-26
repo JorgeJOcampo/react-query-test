1d3a5c0d7ae9f142cbc2d6b35b752e3f02cf3de4
Jorge Ocampo <jorge.ocampo@mercadolibre.com>
2022-12-26 07:01:41 -0300

    Update

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -25,20 +25,32 @@ interface ContactForm {
   phone: string
 }
 
-
 // const Contacts: NextPage<{contacts: Contact[]}> = ({contacts}) => {
   const Contacts: NextPage = () => {
   const [form, setForm] = useState<ContactForm>({name: '', email: '', phone: ''});
+  const [name, setName] = useState<string>('');
 
   const query = useQuery<Contact[]>('contacts', getContacts)
 
-  const contacts = query.data;
+  const {data: contacts, isLoading, isError} = query;
 
   const mutation = useMutation(addContact, {
     onSuccess: () => {
       // Invalidate and refetch
       queryClient.invalidateQueries('contacts')
     },
+  })
+
+  const updateMutation = useMutation(updateContactName, {
+    onSuccess: (data) => {
+      // console.log('data', data)
+      // const a = queryClient.getQueryData(['contacts', {id: data.id}])
+      // console.log('a', a)
+      // queryClient.setQueryData(['contacts', {id: data.id}], data)
+      queryClient.invalidateQueries('contacts')
+    },
+    onError: (e) => console.error(e),
+    onSettled: (data, error, variables, context) => console.log('This log works from success or error')
   })
 
   // Function to handle form submissions
@@ -54,8 +66,16 @@ interface ContactForm {
     setForm({...form, [name]: value});
   }
 
-  if(!contacts?.length){
+  const handleUpdate = (contact: Contact) => {
+    updateMutation.mutate(contact)
+  }
+
+  if(isLoading){
     <div>Loading...</div>
+  }
+
+  if(mutation.isLoading){
+    <div>Creating Contact...</div>
   }
 
   return (
@@ -69,7 +89,11 @@ interface ContactForm {
         </form>
         <ul>
           {contacts?.map(contact => (
-            <li key={contact.name}>{contact.name} ({contact.email})</li>
+            <div key={contact.id}>
+              <li key={contact.name}>{contact.name} ({contact.email})</li>
+              <input onChange={event => setName(event.target.value)}/>
+              <button onClick={()=> handleUpdate({...contact, name})}>Edit</button>
+            </div>
           ))}
         </ul>
       </div>
@@ -79,16 +103,16 @@ interface ContactForm {
 const getContacts = (): Promise<Contact[]> => fetch('/api/contacts')
   .then(data => data.json())
 
-const addContact = (contact: ContactForm): Promise<Contact> => fetch('/api/contacts',{
+const addContact = (contact: ContactForm): Promise<Contact> => fetch('/api/contacts', {
   method: 'post',
   body: JSON.stringify(contact),
   headers: {'Content-Type': 'application/json'}
 }).then(data => data.json())
 
-// Contacts.getInitialProps = async () => {
-//   // Load the contacts from a file
-//   const contacts = await fetch('/api/contacts');
-//   return {contacts};
-// }
+const updateContactName = (contact: Contact): Promise<Contact> => fetch('/api/contacts/' + contact.id, {
+  method: 'put',
+  body: JSON.stringify(contact),
+  headers: {'Content-Type': 'application/json'}
+}).then(data => data.json())
 
 export default Contacts;
